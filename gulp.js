c11bcf3648c3c1a61407d765cd0574d7c74ba368
ukatama <dev.ukatama@gmail.com>
2016-02-13 22:38:48 +0900

    Refactor

--- gulp.js
+++ gulp.js
@@ -1,34 +1,34 @@
-/* eslint strict: 0 */
-/* eslint no-sync: 0 */
-module.exports = function(options) {
-    'use strict';
+'use strict';
 
-    if (!options) options = {};
+const _ = require('lodash');
+const browserify = require('browserify');
+const fs = require('fs');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+const liveserver = require('gulp-live-server');
+const rename = require('gulp-rename');
+const sequence = require('gulp-sequence');
+const sloc = require('gulp-sloc');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const gutil = require('gulp-util');
+const jest = require('jest-cli');
+const jsonlint = require('jsonlint');
+const path = require('path');
+const through = require('through2');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const watchify = require('watchify');
 
-    const browserify = require('browserify');
-    const fs = require('fs');
-    const gulp = require('gulp');
-    const babel = require('gulp-babel');
-    const eslint = require('gulp-eslint');
-    const liveserver = require('gulp-live-server');
-    const rename = require('gulp-rename');
-    const sloc = require('gulp-sloc');
-    const sourcemaps = require('gulp-sourcemaps');
-    const uglify = require('gulp-uglify');
-    const gutil = require('gulp-util');
-    const jest = require('jest-cli');
-    const path = require('path');
-    const source = require('vinyl-source-stream');
-    const buffer = require('vinyl-buffer');
-    const watchify = require('watchify');
-
-    const server = liveserver.new('.');
-
-    const config = {};
-    config.eslint = {
-        extends: path.join(__dirname, '.eslintrc'),
-    };
-    config.babel = {
+const DefaultOptions = {
+    browser: true,
+    server: true,
+    eslint: {
+        default: { extends: path.join(__dirname, 'eslint/default.yml') },
+        jest: { extends: path.join(__dirname, 'eslint/jest.yml') },
+    },
+    babel: {
         presets: [
             'react',
             'es2015',
@@ -36,56 +36,76 @@ module.exports = function(options) {
         ],
         sourceMaps: 'inline',
         sourceRoot: 'src'
-    };
-    config.browserify = {
+    },
+    browserify: {
         entries: ['src/browser'],
         debug: true,
         transform: [
             [
                 'babelify',
-                {
-                    presets: config.babel.presets,
-                },
+                {}
             ],
         ],
-    };
+    },
+    src: {
+        config: 'config/*.json',
+        src: 'src/**/*.js',
+        tests: 'src/**/__tests__/**/*.js',
+        mocks: '__mocks__/**/*.js',
+        browser: 'src/browser/**/*.js',
+        server: 'src/server/**/*.js',
+    },
+};
 
-    gulp.task('default', ['build']);
+function common(opts) {
+    const filter = (deps) =>
+        deps.filter((dep) => {
+            if (dep.match(/:browser$/) && !opts.browser) return false;
+            else if (dep.match(/:server$/) && !opts.server) return false;
+            return true;
+        });
 
-    gulp.task('lint', ['eslint']);
+    gulp.task('default', ['build', 'test']);
 
-    gulp.task('build', ['build:server', 'build:browser']);
-    gulp.task('build:server', ['babel']);
-    gulp.task('build:browser', ['uglify']);
+    gulp.task('lint', ['eslint', 'jsonlint']);
 
-    gulp.task('test', ['eslint', 'jest']);
+    gulp.task('build', filter(['build:server', 'build:browser', 'sloc']));
+    gulp.task('test', ['eslint', 'jest', 'sloc', 'jsonlint']);
 
-    gulp.task('serve', ['server']);
-
-    gulp.task('watch', ['watch:server', 'watch:browser', 'sloc'], () => {
-        gulp.watch(['.eslintrc', 'gulpfile.js'], ['eslint']);
-        gulp.watch('src/**/*', ['sloc']);
+    gulp.task('watch', filter(['watch:server', 'watch:browser']), () => {
+        gulp.watch([opts.src.src], ['babel']);
+        gulp.watch([opts.src.src, `!${opts.src.tests}`], ['eslint:default']);
+        gulp.watch([opts.src.tests, opts.src.mocks], ['eslint:jest']);
+        gulp.watch([opts.src.src, opts.src.mocks], ['jest']);
+        gulp.watch([opts.src.src], ['sloc']);
+        gulp.watch([opts.src.config], ['jsonlint']);
     });
-    gulp.task('watch:server', ['jest', 'server'], () => {
-        gulp.watch(['src/**/*', '!src/browser/**/*', 'config/**/*'], ['jest', 'server']);
-        gulp.watch(
-            ['dist/**/*', 'public/**/*', 'views/**/*'],
-            (file) => server.notify(file)
-        );
-    });
-    gulp.task('watch:browser', ['jest', 'wuglify'], () =>
-        gulp.watch(['src/**/*', '!src/server/**/*', 'config/**/*'], ['jest', 'wuglify']));
 
-    gulp.task('eslint', () =>
-        gulp.src(['src/**/*.js', 'gulpfile.js'])
-            .pipe(eslint(config.eslint))
+    gulp.task('eslint', ['eslint:default', 'eslint:jest'])
+    gulp.task('eslint:default', () =>
+        gulp.src([opts.src.src, `!${opts.src.tests}`])
+            .pipe(eslint(opts.eslint.default))
+            .pipe(eslint.format())
+            .pipe(eslint.failAfterError())
+    );
+    gulp.task('eslint:jest', () =>
+        gulp.src([opts.src.tests])
+            .pipe(eslint(opts.eslint.jest))
             .pipe(eslint.format())
             .pipe(eslint.failAfterError())
     );
 
     gulp.task('sloc', () =>
-        gulp.src('src/**/*.js')
+        gulp.src([opts.src.src])
             .pipe(sloc())
+    );
+
+    gulp.task('jsonlint', () =>
+        gulp.src([opts.src.config])
+            .pipe(through.obj((file, encode, next) => {
+                jsonlint.parse(file.contents.toString(encode));
+                next();
+            }))
     );
 
     gulp.task(
@@ -118,63 +138,10 @@ module.exports = function(options) {
     );
 
     gulp.task(
-        'babel', ['eslint', 'sync-lib'],
-        () => gulp.src('src/**/*.js')
-            .pipe(babel(config.babel))
+        'babel', ['sync-lib'],
+        () => gulp.src([opts.src.src])
+            .pipe(babel(opts.babel))
             .pipe(gulp.dest('lib'))
-    );
-
-    const bundle = function(b) {
-        if (options.browser === false) {
-            return (next) => {
-                gutil.log('Skip browser');
-                next();
-            };
-        }
-
-        return function() {
-            return b.bundle()
-                .on('error', (e) => {
-                    throw e;
-                })
-                .pipe(source('browser.js'))
-                .pipe(buffer())
-                .pipe(sourcemaps.init({loadMaps: true}))
-                .pipe(sourcemaps.write('.'))
-                .pipe(gulp.dest('dist/js'));
-        };
-    };
-    const w = watchify(browserify(Object.assign(
-        {},
-        watchify.args,
-        config.browserify
-    )));
-
-    w.on('update', bundle);
-    w.on('log', gutil.log);
-    gulp.task('watchify', ['eslint'], bundle(w));
-    gulp.task('browserify', ['eslint'], bundle(browserify(config.browserify)));
-
-    gulp.task('uglify', ['browserify'], () =>
-        gulp.src('dist/js/browser.js')
-            .pipe(rename({
-              extname: '.min.js'
-            }))
-            .pipe(sourcemaps.init({loadMaps: true}))
-            .pipe(uglify())
-            .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest('dist/js'))
-    );
-
-    gulp.task('wuglify', ['watchify'], () =>
-        gulp.src('dist/js/browser.js')
-            .pipe(rename({
-              extname: '.min.js'
-            }))
-            .pipe(sourcemaps.init({loadMaps: true}))
-            .pipe(uglify())
-            .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest('dist/js'))
     );
 
     gulp.task('jest', ['babel'], (next) => {
@@ -186,9 +153,80 @@ module.exports = function(options) {
             next(!succeeded && new Error('Test failured'));
         });
     });
+}
+
+function browser(opts) {
+    function bundle(b) {
+        return () =>
+            b.bundle()
+                .on('error', (e) => {
+                    throw e;
+                })
+                .pipe(source('browser.js'))
+                .pipe(buffer())
+                .pipe(sourcemaps.init({loadMaps: true}))
+                .pipe(sourcemaps.write('.'))
+                .pipe(gulp.dest('dist/js'));
+    }
+
+    const w = watchify(browserify(Object.assign(
+        {},
+        watchify.args,
+        opts.browserify
+    )));
+
+    gulp.task('build:browser', ['uglify:build']);
+
+    gulp.task('watch:browser', ['uglify:watch'], () =>
+        gulp.watch([opts.src.src, `!${opts.src.server}`, `!${opts.src.tests}`], ['uglify:watch']));
+
+    w.on('update', bundle);
+    w.on('log', gutil.log);
+    gulp.task('watchify', bundle(w));
+    gulp.task('browserify', bundle(browserify(opts.browserify)));
+
+    gulp.task('uglify:common', () =>
+        gulp.src('dist/js/browser.js')
+            .pipe(rename({
+              extname: '.min.js'
+            }))
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('dist/js'))
+    );
+    gulp.task('uglify:build', (next) => sequence('browserify', 'uglify:common', next));
+    gulp.task('uglify:watch', (next) => sequence('watchify', 'uglify:common', next));
+}
+
+function server(opts) {
+    const app = liveserver.new('.');
+
+    gulp.task('serve', ['server']);
+
+    gulp.task('build:server', ['babel']);
+    gulp.task('watch:server', ['server'], () => {
+        gulp.watch(
+            [opts.src.src, opts.src.config, `!${opts.src.browser}`, `!${opts.src.tests}`,],
+            ['server']
+        );
+        gulp.watch(
+            ['dist/**/*', 'public/**/*', 'views/**/*'],
+            (file) => app.notify(file)
+        );
+    });
 
     gulp.task('server', ['babel'], (next) => {
-        server.start();
+        app.start();
         next();
     });
+}
+
+module.exports = function(opts) {
+    opts = _.defaultsDeep(opts || {}, DefaultOptions);
+    opts.browserify.transform[0][1].presets = opts.babel.presets;
+
+    common(opts);
+    if (opts.browser) browser(opts);
+    if (opts.server) server(opts);
 };
